99c88500eef7ea16b4c42fc74d150f237708f3da
Stefan Heiden <stefanheiden@gmail.com>
2024-04-12 22:16:25 +0200

    adapt keymap

--- keyboards/sofle_choc/keymaps/polpettone/keymap.c
+++ keyboards/sofle_choc/keymaps/polpettone/keymap.c
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT(
-    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,     KC_7,     KC_8,    KC_0,    KC_9,    KC_0,
-    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                      KC_LCTL, KC_LGUI,KC_LALT,  TG(1),   KC_SPC,    KC_ENT , KC_EQL,   KC_MINS,  KC_LBRC, KC_RBRC
+    KC_ESC,   KC_1,   KC_2,    KC_3,   KC_4,    KC_5,                           KC_6,     KC_7,     KC_8,    KC_0,    KC_9,    KC_0,
+    KC_TAB,   KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,                           KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_LSFT,  KC_A,   KC_S,    KC_D,   KC_F,    KC_G,                           KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LCTL,  KC_Z,   KC_X,    KC_C,   KC_V,    KC_B,      KC_MUTE,    KC_MPLY, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                      KC_CAPS, TG(1),  KC_LALT, KC_LGUI,   KC_SPC,    KC_ENT , KC_EQL,   KC_MINS,  KC_LBRC, KC_RBRC
 ),
 
 /*
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [1] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_0,     KC_9,    KC_0,
-    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,     KC_EQL,  KC_MINS,
-    KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,  KC_MPLY, KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_TRNS,
-                   KC_RALT, KC_LGUI, KC_LCMD, TG(1),   KC_SPC,   KC_ENT,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_GRV,   KC_TRNS,   KC_2,    KC_3,    KC_4,      KC_5,                          KC_6,     KC_7,     KC_8,    KC_0,     KC_9,    KC_0,
+    KC_ESC,   KC_TRNS,   KC_W,    KC_E,    KC_R,      KC_T,                          KC_Y,     KC_U,     KC_I,    KC_O,     KC_EQL,  KC_MINS,
+    KC_TAB,   KC_TRNS,   KC_S,    KC_D,    KC_F,      KC_G,                          KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_TRNS,   KC_X,    KC_C,    KC_V,      KC_B,       KC_MUTE,  KC_MPLY, KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_TRNS,
+                         KC_RALT, TG(1),   KC_TRNS,  KC_TRNS,   KC_SPC,   KC_ENT,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
 )
 
 };
